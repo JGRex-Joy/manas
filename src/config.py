39c417aa0bb750b5426ea964de6ad7d274ce471d
JGRex-Joy <omurkulovamir2008@gmail.com>
2026-03-05 14:57:11 +0600

    deployment

--- src/config.py
+++ src/config.py
@@ -30,8 +30,8 @@ class Settings(BaseSettings):
     PREFETCH_MULTIPLIER: int = 3
     
     # Chunking
-    CHUNK_SIZE: int = 600
-    CHUNK_OVERLAP: int = 100
+    CHUNK_SIZE: int = 800
+    CHUNK_OVERLAP: int = 120
     
     class Config:
         env_file = ".env"
